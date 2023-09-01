57f61efb28ccdde5e0284e6aa2fb7c6d90001d8d
Andrew <cybershell@cybershell.xyz>
2023-09-01 13:47:36 -0500

    v2.7
    - Make audio play asynchronously and add functionality to stop after a time set individually
    - Reduced RAM usage by removing hardware Serial object and globals

--- Fire-Truck.ino
+++ Fire-Truck.ino
@@ -14,18 +14,7 @@
 
 void setup() {
   // Sets up the baud rate of the Arduino at 9600
-  Serial.begin(baud);
-
-  // Debug message
-  Serial.println("Serial started at 9600");
-  Serial.println("");
-
-  // Displays information about the sketch file and what day it got uploaded
-  Serial.print("Sketch:   ");
-  Serial.println(__FILE__);
-  Serial.print("Uploaded: ");
-  Serial.println(__DATE__);
-  Serial.println();
+  // Serial.begin(baud);
 
   // Sets up the baud rate of the HM-10 bluetooth module and prints a debug message
   BTsetup(baud);
@@ -34,154 +23,136 @@ void setup() {
   Servo1.attach(servoPin);
   delay(500);
   
-  // Sets up the speaker pin
-  // audio.speakerPin = speakerPin;
-  // pinMode(speakerPin, OUTPUT);
-
-
   // Function that will initialize the DC motors
   initDC();
 
-  // Initalize SD card
-  initSDCard();
 
   // Initialize the water pump port
-  pinMode(water, OUTPUT);
-  digitalWrite(water, HIGH);
+  pinMode(A5, OUTPUT);
+  digitalWrite(A5, HIGH);
 
   
-  // Comment the following two blocks to make servo work
-
-  // Debug message in case the card does not get read
-  // if (!SD.begin(SDpin)) {
-  //   Serial.println("Card cannot be read");
-  //   Serial.println("");
-  // } else {
-  //   Serial.println("Card has been read");
-  //   Serial.println("");
-  // }
-
-  // // The following code is to make sure that the speaker works, so it will play a clown horn to test
-  // audio.setVolume(7);
-  // audio.play("6.wav");
-  // if (!audio.isPlaying()) {
-  //   Serial.println("Audio is not playing");
-  //   Serial.println("");
-  // } else {
-  //   Serial.println("Audio is playing");
-  //   Serial.println("");
-  // }
   
   // Start battery sensing
   batt.begin(BOARD_REF_VOLTAGE, 3.2, &sigmoidal);
   batt.onDemand(ACTIVATION_PIN, HIGH);
   delay(500);
 
-  servoAngle = Servo1.read();
-  Serial.println("Program started!");
+  // Initalize Wave Shield
+  initShield();
+
+  // Serial.println("Program started!");
 }
 
 void loop() {
+  bool newData;
+
 
   currentTime = millis();
 
-  if (motorsMoving && (currentTime - timeMotorsEngaged >= timeToStopMotor))
+  // motors will stop after 30 * 1000 ms = 30000 ms = 30 s
+  if (motorsMoving && (currentTime - timeMotorsEngaged >= 30000))
   {
     brake();
     motorsMoving = false;
   }
+  if (wave.isplaying && (currentTime - timeSoundStarted >= timeToStopPlayingSound ))
+  {
+    // Go back to the begining of the filesystem
+    wave.stop();
+    file.close();
+    root.rewind();
+  }
+  
 
   // Read from the Bluetooth module and send to the Arduino serial monitor
   BTSerial.listen();
   if(BTSerial.available())
   {
     data = BTSerial.read();
-    Serial.print(data);
+    newData = true;
   }
   
-  // If-else statements that'll call the specific function if the condition gets met
-  if (data == 'P') {
-    stopPlayback();
-    data = NULL;
-  } else if (data == 'A') {
-    soundbyte1();
-    data = NULL;
-  } else if (data == 'C') {
-    soundbyte2();
-    data = NULL;
-  } else if (data == 'C') {
-    motorTest();
-    data = NULL;
-  } else if (data == 'T') {
-    motorTest2();
-    data = NULL;
-  } else if (data == 'F') {
-    engageMotors(255, "forward");
-  } else if (data == 'B') {
-    engageMotors(255, "backward");
-  } else if (data == 'S') {
-    brake();
-    motorsMoving = false;
-    data = NULL;
-  } else if (data == 'W') {
-    waterPump();
-  } else if (data == 'V') {
-    printVoltage();
-    data = NULL;
-  }
-  // Turn the servo to 0 degrees
-  if (data == 'R') {
-    
-    servoAngle = Servo1.read();
-
-    for (servoAngle; servoAngle >= 0; servoAngle--)
-    {
-      Servo1.write(servoAngle);
-      delay(servoDelay);
-      CHAR_AVAILABLE_BREAK;
+  if (newData)
+  {  
+    // If-else statements that'll call the specific function if the condition gets met
+    if (data == 'P') {
+      stopPlayback();
+      data = NULL;
+    } else if (data == 'A') {
+      soundbyte1();
+      data = NULL;
+    } else if (data == 'C') {
+      soundbyte2();
+      data = NULL;
+    } else if (data == 'F') {
+      // engageMotors(255, "forward");
+    } else if (data == 'B') {
+      // engageMotors(255, "backward");
+    } else if (data == 'S') {
+      // brake();
+      // motorsMoving = false;
+      data = NULL;
+    } else if (data == 'W') {
+      waterPump();
+    } else if (data == 'V') {
+      printVoltage();
+      data = NULL;
     }
-    data = NULL;
-  }
-  // Turn the servo perpendicular (90 degrees)
-  if (data == 't') {
-    
-    servoAngle = Servo1.read();
-    
-    if (servoAngle >= 0 && servoAngle < 89)
-    {
-      for (servoAngle; servoAngle < 90; servoAngle++)
+    // Turn the servo to 0 degrees
+    if (data == 'R') {
+      
+      servoAngle = Servo1.read();
+
+      for (servoAngle; servoAngle >= 0; servoAngle--)
       {
         Servo1.write(servoAngle);
         delay(servoDelay);
-        CHAR_AVAILABLE_BREAK;
+        if (BTSerial.available()) break;
       }
-    } else if (servoAngle > 89)
-    {
-      for (servoAngle; servoAngle > 90; servoAngle--)
+      data = NULL;
+    }
+    // Turn the servo perpendicular (90 degrees)
+    if (data == 't') {
+      
+      servoAngle = Servo1.read();
+      
+      if (servoAngle >= 0 && servoAngle < 89)
+      {
+        for (servoAngle; servoAngle < 90; servoAngle++)
+        {
+          Servo1.write(servoAngle);
+          delay(servoDelay);
+          if (BTSerial.available()) break;
+        }
+      } else if (servoAngle > 89)
+      {
+        for (servoAngle; servoAngle > 90; servoAngle--)
+        {
+          Servo1.write(servoAngle);
+          delay(servoDelay);
+          if (BTSerial.available()) break;
+        }
+      } else {
+        Servo1.write(90);
+      }
+      data = NULL;
+    }
+    // Turn the servo to 180 degrees
+    if (data == 'L') {
+      
+      servoAngle = Servo1.read();
+      
+      for (servoAngle; servoAngle <= 180; servoAngle++)
       {
         Servo1.write(servoAngle);
         delay(servoDelay);
-        CHAR_AVAILABLE_BREAK;
+        if (BTSerial.available()) break;
       }
-    } else {
-      Servo1.write(90);
+      data = NULL;
     }
-    data = NULL;
-  }
-  // Turn the servo to 180 degrees
-  if (data == 'L') {
-    
-    servoAngle = Servo1.read();
-    
-    for (servoAngle; servoAngle <= 180; servoAngle++)
-    {
-      Servo1.write(servoAngle);
-      delay(servoDelay);
-      CHAR_AVAILABLE_BREAK;
-    }
-    data = NULL;
-  }
 
+  }
   // TODO: decide on how to implement backwards and forwards
 
   
@@ -190,8 +161,8 @@ void loop() {
 // Function that sets up bluetooth
 void BTsetup(int baud) {
   BTSerial.begin(baud);
-  Serial.println("BTserial started at 9600");
-  Serial.println("");
+  // Serial.println("BTserial started at 9600");
+  // Serial.println("");
 }
 
 // Function to initialize DC motors and start them in the "OFF" state
@@ -211,12 +182,14 @@ void initDC() {
 // Function used to play the first soundbyte
 void soundbyte1() {
   playSound("3.wav");
+  // wave.stop();
   data = NULL;
 }
 
 // Function used to play the second soundbyte
 void soundbyte2() {
   playSound("4.wav");
+  // wave.stop();
   data = NULL;
 }
 
@@ -228,57 +201,24 @@ void stopPlayback() {
 
 // Function for turning the water pump on and off
 void waterPump() {
-  digitalWrite(water, LOW);
+  digitalWrite(A5, LOW);
   delay(1000);
-  digitalWrite(water, HIGH);
+  digitalWrite(A5, HIGH);
 }
 
-// Function used for testing the DC motors
-void motorTest() {
-
-  // Set the speed for the motors
-  analogWrite(enA_B, 100);
-
-  //Test the DC motors
-  digitalWrite(in1_3, HIGH); 
-  digitalWrite(in2_4, LOW);
-  delay(5000);
-
-  digitalWrite(in1_3, LOW); 
-  digitalWrite(in2_4, HIGH);
-  delay(5000);
-}
-
-// Function to test the DC motors by slowly raising their speed over time
-void motorTest2() {
-  digitalWrite(in1_3, HIGH);
-  digitalWrite(in2_4, LOW);
-
-  // Accelerate from zero to maximum speed
-  for (int i = 0; i < 256; i++) {
-    analogWrite(enA_B, i);
-    delay(20);
-  }
-
-  // Decelerate from maximum speed to zero
-  for (int i = 255; i > 0; --i) {
-    analogWrite(enA_B, i);
-    delay(20);
-  }
-}
 
 // Function that moves the DC motors clockwise/forward
 void forward(int speed) {
-  analogWrite(enA_B, speed);
-  digitalWrite(in1_3, HIGH);
-  digitalWrite(in2_4, LOW);
+  // analogWrite(enA_B, speed);
+  // digitalWrite(in1_3, HIGH);
+  // digitalWrite(in2_4, LOW);
 }
 
 // Function that moves the DC motors counter-clockwise/backward
 void backward(int speed) {
-  analogWrite(enA_B, speed);
-  digitalWrite(in1_3, LOW); 
-  digitalWrite(in2_4, HIGH);
+  // analogWrite(enA_B, speed);
+  // digitalWrite(in1_3, LOW); 
+  // digitalWrite(in2_4, HIGH);
 }
 
 // engageMotors changes the motors' speed to speed and directon to dir
@@ -289,16 +229,16 @@ void engageMotors(int speed, String dir) {
     backward(speed);
   }
 
-  motorsMoving = true;
-  timeMotorsEngaged = currentTime;
-  BTSerial.print("Time motors engaged: ");
-  BTSerial.println(timeMotorsEngaged);
-  data = NULL;
+  // motorsMoving = true;
+  // timeMotorsEngaged = currentTime;
+  // BTSerial.print("Time motors engaged: ");
+  // BTSerial.println(timeMotorsEngaged);
+  // data = NULL;
 }
 
 void brake() {
-  digitalWrite(in1_3, LOW);
-  digitalWrite(in2_4, LOW);
+  // digitalWrite(in1_3, LOW);
+  // digitalWrite(in2_4, LOW);
 }
 
 // printVoltage calculates and sends the battery level over the Bluetooth connection 
@@ -310,17 +250,20 @@ void printVoltage() {
   BTSerial.println(batt.level());
 }
 
-// initalize and check SD card
-void initSDCard() {
+// initalize and check Wave Shield and SD card
+void initShield() {
 
   // Set the output pins for the DAC control.
-  pinMode(CSpin, OUTPUT);
-  pinMode(MOSIpin, OUTPUT);
-  pinMode(MISOpin, OUTPUT);
-  pinMode(SCKpin, OUTPUT);
+  pinMode(SS, OUTPUT);
+  pinMode(SCK, OUTPUT);
+  pinMode(MOSI, OUTPUT);
+  pinMode(MISO, OUTPUT);
+  
+  // Serial.print("Freeram: ");
+  // Serial.println(FreeRam());
   
   if (!card.init()) {         //play with 8 MHz spi (default faster!)  
-    Serial.println("Card init. failed!");  // Something went wrong, lets print out why
+    // Serial.println("Card init. failed!");  // Something went wrong, lets print out why
     // sdErrorCheck();
     // while(1);                            // then 'halt' - do nothing!
   }
@@ -332,36 +275,34 @@ void initSDCard() {
       break;                             // we found one, lets bail
   }
   if (part == 5) {                       // if we ended up not finding one  :(
-    Serial.println("No valid FAT partition!");
+    // Serial.println("No valid FAT partition!");
     // sdErrorCheck();      // Something went wrong, lets print out why
-    // while(1);                            // then 'halt' - do nothing!
+    while(1);                            // then 'halt' - do nothing!
   }
-  
-  // Let's tell the user about what we found
-  Serial.println("Using partition ");
-  Serial.print(part, DEC);
-  Serial.println(", type is FAT");
-  Serial.println(vol.fatType(),DEC);     // FAT16 or FAT32?
 
   // Open the root of the SD Card
   if (!root.openRoot(vol)) {
-    Serial.println("Couldn't open root");
+    // Serial.println("Couldn't open root");
   }
 }
 
 // playSound reads a file in the root directory and plays it over the speaker
-void playSound(char soundFile[12]) {
-  FatReader file;
+void playSound(char soundFile[11]) {
+  // FatReader file;
 
   if (!file.open(root, soundFile)) {       // open the file in the directory
-      Serial.println("file.open failed");  // something went wrong :(
+      // Serial.println("file.open failed");  // something went wrong :(
       while(1);                            // halt
   }
 
   // create wave object for the file and play if no error
   if (wave.create(file))
   {
+    BTSerial.print("Freeram: ");
+    BTSerial.println(FreeRam());
     wave.play();
+    timeSoundStarted = millis();
+    timeToStopPlayingSound = 10 * 1000;
   } else {
     BTSerial.println("Failed to read file ");
     BTSerial.print(soundFile);
@@ -373,6 +314,4 @@ void playSound(char soundFile[12]) {
   } else {
     BTSerial.println("Audio not playing.");
   }
-  // Go back to the begining of the filesystem
-  root.rewind();
 }
--- firetruck.h
+++ firetruck.h
@@ -8,32 +8,27 @@
 // Library used to control the servo
 #include <PWMServo.h>
 
-// Library for connecting to the SD card adapter
-#include <SPI.h>
-
 // Library that Andrew Woodlee modified for reading battery level using the relay
 #include "Battery.h"
 
 // Creates the bluetooth object with the reciever and transmitter pins as the arguments
-const int btRxPin = A3;
-const int btTxPin = A4;
+const int btRxPin = A4;
+const int btTxPin = A3;
 SoftwareSerial BTSerial(btRxPin, btTxPin);
 
 // Macro to determine if character is avaliable to be read over Bluetooth
 #define CHAR_AVAILABLE BTSerial.available()
-#define CHAR_AVAILABLE_BREAK if (BTSerial.available()) break
 
 /*
   Motor configuration
 */
 
-// The constants that will be used with both DC motors
-const int in1_3 = 7;
-const int in2_4 = 6;
-
-
 // The constants used for controlling the enabling pins
-const int enA_B = 5; 
+const int enA_B = 5;
+
+// The constants that will be used with both DC motors
+const int in1_3 = 6;
+const int in2_4 = 7;
 
 
 /*
@@ -52,13 +47,7 @@ PWMServo Servo1;
   Constants used for SPI Bus Ports
 */
 
-const int CSpin = SS;
-const int SCKpin = SCK;
-const int MOSIpin = MOSI;
-const int MISOpin = MISO;
-
 const int speakerPin = 9;
-const int SDpin = 10;
 
 // Variable used for recieving and transmitting data
 char data;
@@ -67,23 +56,24 @@ char data;
 const int baud = 9600;
 
 // Constant used for the water pump port
-const int water = A5;
+// const int water = A5;
 
-// Variables for events
-const unsigned long timeToStopMotor = 1000 * 30; // 30 seconds
+// // Variables for events
 unsigned long currentTime;
 unsigned long timeMotorsEngaged;
 boolean motorsMoving = false;
 
+unsigned long timeSoundStarted;
+unsigned long timeToStopPlayingSound;
+
 // Battery sensing setup and constants
 #define SENSE_PIN A0
 #define ACTIVATION_PIN A1
-#define MAX_VOLTAGE 12000
-#define MIN_VOLTAGE 10000
 // This board's reference voltage in milivolts - CHANGE FOR EVERY BOARD, details in project manual
-#define BOARD_REF_VOLTAGE 5140
 // Battery class initialized with values for sensing battery level
-Battery batt = Battery(MIN_VOLTAGE, MAX_VOLTAGE, SENSE_PIN);
+// Parameters: min voltage, max voltage, sense pin
+#define BOARD_REF_VOLTAGE 5140
+Battery batt = Battery(10000, 12000, SENSE_PIN);
 
 // SD card and sound objects
 
@@ -91,3 +81,4 @@ SdReader card;    // This object holds the information for the card
 FatVolume vol;    // This holds the information for the partition on the card
 FatReader root;   // This holds the information for the filesystem on the card
 WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
+FatReader file;
